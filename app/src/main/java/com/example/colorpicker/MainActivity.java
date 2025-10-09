7358bf626efdb6b9c4cb6b795d9104e505aac310
Daron <daronbaltazar@gmail.com>
2025-10-09 11:51:13 -0500

    added sms receiver
    added sms receiver things

--- app/src/main/java/com/example/colorpicker/MainActivity.java
+++ app/src/main/java/com/example/colorpicker/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.colorpicker;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity implements OnMessageSendListener {
 
@@ -19,14 +24,22 @@ public class MainActivity extends AppCompatActivity implements OnMessageSendList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Log.i("TAG", String.valueOf(ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS)));
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED){
+            String[] perm = new String[]{Manifest.permission.RECEIVE_SMS};
+            ActivityCompat.requestPermissions(this, perm, 101);
+        }
+
         if (savedInstanceState == null) {
             colorsModel = new ViewModelProvider(this).get(ColorsViewModel.class);
             fg = getSupportFragmentManager();
             FragmentTransaction trans = fg.beginTransaction();
             ColorPickFragment cf = new ColorPickFragment();
             trans.add(R.id.colorFragment, cf, "colorFrag");
-            //ColorListFragment cl = new ColorListFragment();
-           // trans.add(R.id.ListFragment, cl, "listFrag");
+            BlColorListFragment cl = new BlColorListFragment();
+            trans.add(R.id.ListFragment, cl, "listFrag");
             trans.commit();
 
 
